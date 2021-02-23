70eb423e62cbaef9a87d1101a76bc871d629ede9
rikuyu <rikuyu5884@gmail.com>
2021-02-24 04:13:15 +0900

    define method

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-//import kotlinx.android.synthetic.main.activity_main.*
-//import net.objecthunter.exp4j.ExpressionBuilder
+import androidx.appcompat.app.AppCompatActivity
+
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,26 +31,71 @@ class MainActivity : AppCompatActivity() {
         val btnRightB: Button = findViewById(R.id.btnRightB)
         val btnClear: Button = findViewById(R.id.btnClear)
         val btnEqual: Button = findViewById(R.id.btnEqual)
+        val btnDot: Button = findViewById(R.id.btnDot)
 
-        btn00.setOnClickListener{}
-        btn0.setOnClickListener {}
-        btn1.setOnClickListener {}
-        btn2.setOnClickListener {}
-        btn3.setOnClickListener {}
-        btn4.setOnClickListener {}
-        btn5.setOnClickListener {}
-        btn6.setOnClickListener {}
-        btn7.setOnClickListener {}
-        btn8.setOnClickListener {}
-        btn9.setOnClickListener {}
+        btn00.setOnClickListener { appendText(true, "00") }
+        btn0.setOnClickListener { appendText(true, "0") }
+        btn1.setOnClickListener { appendText(true, "1") }
+        btn2.setOnClickListener { appendText(true, "2") }
+        btn3.setOnClickListener { appendText(true, "3") }
+        btn4.setOnClickListener { appendText(true, "4") }
+        btn5.setOnClickListener { appendText(true, "5") }
+        btn6.setOnClickListener { appendText(true, "6") }
+        btn7.setOnClickListener { appendText(true, "7") }
+        btn8.setOnClickListener { appendText(true, "8") }
+        btn9.setOnClickListener { appendText(true, "9") }
+        btnDot.setOnClickListener { appendText(true, ".") }
 
-        btnPlus.setOnClickListener {}
-        btnMinus.setOnClickListener {}
-        btnMultiply.setOnClickListener {}
-        btnDivide.setOnClickListener {}
-        btnLeftB.setOnClickListener {}
-        btnRightB.setOnClickListener {}
-        btnClear.setOnClickListener {}
-        btnEqual.setOnClickListener {}
+        btnPlus.setOnClickListener { appendText(false, "+") }
+        btnMinus.setOnClickListener { appendText(false, "-") }
+        btnMultiply.setOnClickListener { appendText(false, "*") }
+        btnDivide.setOnClickListener { appendText(false, "/") }
+        btnLeftB.setOnClickListener { appendText(false, "(") }
+        btnRightB.setOnClickListener { appendText(false, ")") }
+        btnClear.setOnClickListener { handleClear() }
+        btnEqual.setOnClickListener { calculate() }
+    }
+
+    fun appendText(clear: Boolean, string: String) {
+        val tvInput: TextView = findViewById(R.id.tvInput)
+        val tvOutput: TextView = findViewById(R.id.tvOutput)
+
+        if (clear) {
+            tvOutput.text = ""
+            tvInput.append(string)
+        } else {
+            tvInput.append(tvOutput.text)
+            tvInput.append(string)
+            tvOutput.text = ""
+        }
+    }
+
+    fun handleClear() {
+        val tvInput: TextView = findViewById(R.id.tvInput)
+        val tvOutput: TextView = findViewById(R.id.tvOutput)
+
+        tvInput.text = ""
+        tvOutput.text = ""
+    }
+
+    fun calculate() {
+        val tvInput: TextView = findViewById(R.id.tvInput)
+        val tvOutput: TextView = findViewById(R.id.tvOutput)
+
+        try {
+            val input = ExpressionBuilder(tvInput.text.toString()).build()
+            val output = input.evaluate()
+            val longOutput = output.toLong()
+            if (output == longOutput.toDouble()) {
+                tvOutput.text = longOutput.toString()
+                tvInput.text = ""
+            } else {
+                tvOutput.text = output.toString()
+                tvInput.text = ""
+            }
+
+        } catch (e: Exception) {
+            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+        }
     }
 }
